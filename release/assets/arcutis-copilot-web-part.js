5bf2308b9e4c0a148407baa35ef037ffe1f0fddf
deepak.kumar@chryselys.com <shakyadeep21@gmail.com>
2026-05-14 12:26:35 +0530

    changes for UI

--- release/assets/arcutis-copilot-web-part.js
+++ release/assets/arcutis-copilot-web-part.js
@@ -1,3 +1,139 @@
+define("5840c725-7097-4eb2-8df2-0624f0180459_0.0.1", ["@microsoft/sp-property-pane","ArcutisCopilotWebPartStrings","@microsoft/sp-core-library","@microsoft/sp-webpart-base"], function(__WEBPACK_EXTERNAL_MODULE__26ea__, __WEBPACK_EXTERNAL_MODULE_KQnZ__, __WEBPACK_EXTERNAL_MODULE_UWqr__, __WEBPACK_EXTERNAL_MODULE_br4S__) { return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Set the webpack public path
+/******/ 	(function () {
+/******/ 	  var scripts = document.getElementsByTagName('script');
+/******/ 	  var regex = /arcutis\-copilot\-web\-part\.js/i;
+/******/ 	  var publicPath;
+/******/
+/******/ 	  if (scripts && scripts.length) {
+/******/ 	    for (var i = 0; i < scripts.length; i++) {
+/******/ 	      if (!scripts[i]) continue;
+/******/ 	      var path = scripts[i].getAttribute('src');
+/******/ 	      if (path && path.match(regex)) {
+/******/ 	        publicPath = path.substring(0, path.lastIndexOf('/') + 1);
+/******/ 	        break;
+/******/ 	      }
+/******/ 	    }
+/******/ 	  }
+/******/
+/******/ 	  if (!publicPath) {
+/******/ 	    for (var global in window.__setWebpackPublicPathLoaderSrcRegistry__) {
+/******/ 	      if (global && global.match(regex)) {
+/******/ 	        publicPath = global.substring(0, global.lastIndexOf('/') + 1);
+/******/ 	        break;
+/******/ 	      }
+/******/ 	    }
+/******/ 	  }
+/******/ 	  __webpack_require__.p = publicPath;
+/******/ 	})();
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "IMZD");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "26ea":
+/*!**********************************************!*\
+  !*** external "@microsoft/sp-property-pane" ***!
+  \**********************************************/
+/*! no static exports found */
+/*! exports used: PropertyPaneTextField */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__26ea__;
+
+/***/ }),
+
+/***/ "De5t":
+/*!***************************************************************!*\
+  !*** ./lib/webparts/arcutisCopilot/scripts/copilot-widget.js ***!
+  \***************************************************************/
+/*! no static exports found */
+/*! all exports used */
+/***/ (function(module, exports) {
+
 /**
  * Copilot Widget - Self-contained Chat Widget with WebSocket
  * Works like Intercom/Yellow.ai - just add the script and it works!
@@ -1866,3 +2002,159 @@
   // Auto-initialize
   init()
 })(window, document)
+
+/***/ }),
+
+/***/ "IMZD":
+/*!**************************************************************!*\
+  !*** ./lib/webparts/arcutisCopilot/ArcutisCopilotWebPart.js ***!
+  \**************************************************************/
+/*! exports provided: default */
+/*! all exports used */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _microsoft_sp_core_library__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @microsoft/sp-core-library */ "UWqr");
+/* harmony import */ var _microsoft_sp_core_library__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_microsoft_sp_core_library__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _microsoft_sp_property_pane__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @microsoft/sp-property-pane */ "26ea");
+/* harmony import */ var _microsoft_sp_property_pane__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_microsoft_sp_property_pane__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _microsoft_sp_webpart_base__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @microsoft/sp-webpart-base */ "br4S");
+/* harmony import */ var _microsoft_sp_webpart_base__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_microsoft_sp_webpart_base__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var ArcutisCopilotWebPartStrings__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ArcutisCopilotWebPartStrings */ "KQnZ");
+/* harmony import */ var ArcutisCopilotWebPartStrings__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(ArcutisCopilotWebPartStrings__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _assets_arcutis_icon_png__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./assets/arcutis-icon.png */ "werJ");
+/* harmony import */ var _assets_arcutis_icon_png__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_assets_arcutis_icon_png__WEBPACK_IMPORTED_MODULE_4__);
+var __extends = (undefined && undefined.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+
+
+
+
+
+var ArcutisCopilotWebPart = /** @class */ (function (_super) {
+    __extends(ArcutisCopilotWebPart, _super);
+    function ArcutisCopilotWebPart() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this._widgetInitialized = false;
+        return _this;
+    }
+    ArcutisCopilotWebPart.prototype.render = function () {
+        // Widget renders its own floating UI on document.body; web part host stays minimal.
+        this.domElement.innerHTML = '<div class="arcutisCopilotHost" aria-hidden="true"></div>';
+    };
+    ArcutisCopilotWebPart.prototype.onInit = function () {
+        if (!this._widgetInitialized && !window.CopilotBubbleLoaded) {
+            window.CopilotBubbleConfig = {
+                websocketUrl: 'wss://arcutis-ai-v1-dkgmb6awhxgze5bw.centralus-01.azurewebsites.net/ws',
+                botIconUrl: _assets_arcutis_icon_png__WEBPACK_IMPORTED_MODULE_4___default.a,
+                position: 'bottom-right',
+                primaryColor: '#a67c52',
+                theme: 'light',
+                bubbleText: 'AI',
+            };
+            // Bundled with the web part so SharePoint CSP allows execution (no inline/external script tags).
+            // eslint-disable-next-line @typescript-eslint/no-var-requires
+            __webpack_require__(/*! ./scripts/copilot-widget.js */ "De5t");
+            this._widgetInitialized = true;
+        }
+        return _super.prototype.onInit.call(this);
+    };
+    Object.defineProperty(ArcutisCopilotWebPart.prototype, "dataVersion", {
+        get: function () {
+            return _microsoft_sp_core_library__WEBPACK_IMPORTED_MODULE_0__["Version"].parse('1.0');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    ArcutisCopilotWebPart.prototype.getPropertyPaneConfiguration = function () {
+        return {
+            pages: [
+                {
+                    header: {
+                        description: ArcutisCopilotWebPartStrings__WEBPACK_IMPORTED_MODULE_3__["PropertyPaneDescription"]
+                    },
+                    groups: [
+                        {
+                            groupName: ArcutisCopilotWebPartStrings__WEBPACK_IMPORTED_MODULE_3__["BasicGroupName"],
+                            groupFields: [
+                                Object(_microsoft_sp_property_pane__WEBPACK_IMPORTED_MODULE_1__["PropertyPaneTextField"])('description', {
+                                    label: ArcutisCopilotWebPartStrings__WEBPACK_IMPORTED_MODULE_3__["DescriptionFieldLabel"]
+                                })
+                            ]
+                        }
+                    ]
+                }
+            ]
+        };
+    };
+    return ArcutisCopilotWebPart;
+}(_microsoft_sp_webpart_base__WEBPACK_IMPORTED_MODULE_2__["BaseClientSideWebPart"]));
+/* harmony default export */ __webpack_exports__["default"] = (ArcutisCopilotWebPart);
+
+
+/***/ }),
+
+/***/ "KQnZ":
+/*!***********************************************!*\
+  !*** external "ArcutisCopilotWebPartStrings" ***!
+  \***********************************************/
+/*! no static exports found */
+/*! exports used: BasicGroupName, DescriptionFieldLabel, PropertyPaneDescription */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_KQnZ__;
+
+/***/ }),
+
+/***/ "UWqr":
+/*!*********************************************!*\
+  !*** external "@microsoft/sp-core-library" ***!
+  \*********************************************/
+/*! no static exports found */
+/*! exports used: Version */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_UWqr__;
+
+/***/ }),
+
+/***/ "br4S":
+/*!*********************************************!*\
+  !*** external "@microsoft/sp-webpart-base" ***!
+  \*********************************************/
+/*! no static exports found */
+/*! exports used: BaseClientSideWebPart */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE_br4S__;
+
+/***/ }),
+
+/***/ "werJ":
+/*!*************************************************************!*\
+  !*** ./lib/webparts/arcutisCopilot/assets/arcutis-icon.png ***!
+  \*************************************************************/
+/*! no static exports found */
+/*! exports used: default */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__.p + "arcutis-icon_567f94f47a416e4f852ff3780a16ac45.png";
+
+/***/ })
+
+/******/ })});;
+//# sourceMappingURL=arcutis-copilot-web-part.js.map
